0458e4cb795435e78a934bb9b0217a909aa4a169
Daniel Bermejo Sánchez <37331692+bermejosanchezdaniel@users.noreply.github.com>
2020-04-06 13:13:10 +0200

    Add files via upload

--- D4C_API.py
+++ D4C_API.py
@@ -1,11 +1,16 @@
-from flask import Flask, jsonify, request
+
+
+from flask import Flask, jsonify, request, Response
 from pymongo import MongoClient
 import json
 from bson import ObjectId, json_util
-
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['DEBUG'] = False
+
 
 client = MongoClient("mongodb+srv://test:test@covid-19-0wxmi.mongodb.net/test?retryWrites=true&w=majority")
 db = client.get_database('Drugs')
@@ -20,6 +25,7 @@ class JSONEncoder(json.JSONEncoder):
 
 
 @app.route('/', methods=['GET','POST'])
+@cross_origin()
 def handleRequest():
         if request.method == 'GET':
                 records = user_records.find()
@@ -27,15 +33,18 @@ def handleRequest():
                 for record in records:
                         record.pop('_id', None)
                         response.append(record)
-                return json.dumps(response, default=json_util.default)
+                json_data = json.dumps(response, default=json_util.default)
+                resp = Response(json_data, content_type='application/json; charset=utf-8')
+                return resp
 
         if request.method == 'POST':
                 req_data = request.get_json()
                 user_records.insert_one(req_data)
-                return('', 204)
+
+                return ('',204)
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', ssl_context='adhoc')
+                app.run(host='0.0.0.0', ssl_context='adhoc')
 
 
 
